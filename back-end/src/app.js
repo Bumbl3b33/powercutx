c85c8023ed282d0b2372ed8bc55fc94feccee9d7
Azam Fahmy <azamfahmy@gmail.com>
2022-03-23 12:46:44 +0530

    Add: Security and Compression for Backend

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const compression = require("compression");
+const helmet = require("helmet");
 const routes = require("./routes/routes");
-const app = express();
 const cors = require("cors");
 
+/* Create Application Object */
+const app = express();
+
 /* Middleware */
+app.use(helmet()); //Security for known vulnerabilities
+app.use(compression()); //Compress HTTP headers and body
 app.use(express.json());
 app.use(cors());
 app.use(routes);
--- queries/setup/0_create_db.sql
+++ queries/setup/0_create_db.sql
@@ -3,10 +3,10 @@ GO
 IF NOT EXISTS (
  SELECT name
  FROM sys.databases
- WHERE name = N'dbCut'
+ WHERE name = N'dbPowercuts'
 )
- CREATE DATABASE [dbCut];
+ CREATE DATABASE [dbPowercuts];
 GO
 IF SERVERPROPERTY('ProductVersion') > '12'
- ALTER DATABASE [dbCut] SET QUERY_STORE=ON;
+ ALTER DATABASE [dbPowercuts] SET QUERY_STORE=ON;
 GO
